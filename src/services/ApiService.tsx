b3d0dd40317c13f2e57d74ccac3c455715c48f8c
Cherisher08 <vigu2002ram@gmail.com>
2025-07-26 18:24:03 +0530

    remove tsconfig.app.tsbuildinfo

--- src/services/ApiService.tsx
+++ src/services/ApiService.tsx
@@ -20,6 +20,7 @@ import {
 } from "../types/common";
 
 const apiUrl = import.meta.env.VITE_BACKEND_ENDPOINT;
+console.log("apiUrl: ", apiUrl);
 
 const baseQuery = fetchBaseQuery({
   baseUrl: apiUrl,

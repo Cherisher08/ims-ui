47093a7b4536419ce760251658e63359855ae8cb
MogeshG <gmogesh2003@gmail.com>
2025-09-13 23:15:25 +0530

    orders table fix for sales product

--- src/services/utility_functions.ts
+++ src/services/utility_functions.ts
@@ -1,9 +1,6 @@
-import { ProductDetails } from "../types/order";
+import { ProductDetails } from '../types/order';
 
-export const calculateDiscountAmount = (
-  discountPercent: number,
-  finalAmount: number
-) => {
+export const calculateDiscountAmount = (discountPercent: number, finalAmount: number) => {
   return +((discountPercent / 100.0) * finalAmount).toFixed(2);
 };
 
@@ -11,8 +8,7 @@ export const calculateProductRent = (
   product: ProductDetails,
   isReturnDuration: boolean = false
 ): number => {
-  const { duration, rent_per_unit, order_quantity, order_repair_count } =
-    product;
+  const { duration, rent_per_unit, order_quantity, order_repair_count } = product;
 
   const effectiveQuantity = order_quantity - order_repair_count;
   if (isReturnDuration) return duration;

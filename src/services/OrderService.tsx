82cada33c025b7c0519c03171ac8c07a558e913c
Cherisher08 <vigu2002ram@gmail.com>
2025-09-27 18:03:47 +0530

    feat: add Privacy Policy route and implement WhatsApp message functionality
    - Added a new route for Privacy Policy in the router configuration.
    - Updated OrderService to include a new mutation for sending orders as WhatsApp messages.
    - Introduced DCWhatsappPayload type to define the structure for WhatsApp message payload.
    - Refactored existing API queries and mutations for consistency in string usage.

--- src/services/OrderService.tsx
+++ src/services/OrderService.tsx
@@ -1,74 +1,70 @@
-import { setExpiredRentalOrders } from "../store/OrdersSlice";
-import { PatchPayload } from "../types/common";
-import { RentalOrderInfo } from "../types/order";
-import { rootApi } from "./ApiService";
-import {
-  transformRentalOrderResponse,
-  transformRentalOrderToUTC,
-} from "./transformFunctions";
+import { setExpiredRentalOrders } from '../store/OrdersSlice';
+import { PatchPayload } from '../types/common';
+import { DCWhatsappPayload, RentalOrderInfo } from '../types/order';
+import { rootApi } from './ApiService';
+import { transformRentalOrderResponse, transformRentalOrderToUTC } from './transformFunctions';
 
 export const contactApi = rootApi.injectEndpoints({
   endpoints: (build) => ({
     getRentalOrders: build.query<RentalOrderInfo[], void>({
-      query: () => `orders/rentals`,
-      providesTags: ["Rental"],
+      query: () => 'orders/rentals',
+      providesTags: ['Rental'],
     }),
     createRentalOrder: build.mutation<RentalOrderInfo, RentalOrderInfo>({
       query: (order) => {
         const body = transformRentalOrderToUTC(order);
         return {
-          url: `orders/rentals`,
-          method: "POST",
+          url: 'orders/rentals',
+          method: 'POST',
           body: body,
         };
       },
-      invalidatesTags: ["Rental"],
+      invalidatesTags: ['Rental'],
     }),
     getRentalOrderById: build.query<RentalOrderInfo, string>({
       query: (id) => `orders/rentals/${id}`,
-      transformResponse: (response: RentalOrderInfo) =>
-        transformRentalOrderResponse(response),
-      providesTags: ["Rental"],
+      transformResponse: (response: RentalOrderInfo) => transformRentalOrderResponse(response),
+      providesTags: ['Rental'],
     }),
     updateRentalOrder: build.mutation<RentalOrderInfo, RentalOrderInfo>({
       query: ({ _id, ...order }) => {
         const body = transformRentalOrderToUTC(order);
         return {
           url: `orders/rentals/${_id}`,
-          method: "PUT",
+          method: 'PUT',
           body: body,
         };
       },
-      invalidatesTags: ["Rental"],
+      invalidatesTags: ['Rental'],
     }),
     deleteRentalOrder: build.mutation<void, string>({
       query: (id) => ({
         url: `orders/rentals/${id}`,
-        method: "DELETE",
+        method: 'DELETE',
       }),
-      invalidatesTags: ["Rental"],
+      invalidatesTags: ['Rental'],
     }),
     getExpiredRentalOrders: build.query<RentalOrderInfo[], void>({
-      query: () => `orders/rentals/expired`,
-      providesTags: ["Expired"],
+      query: () => 'orders/rentals/expired',
+      providesTags: ['Expired'],
       async onQueryStarted(_, { dispatch, queryFulfilled }) {
         try {
           const { data } = await queryFulfilled;
           dispatch(setExpiredRentalOrders(data));
         } catch (err) {
           if (
-            typeof err === "object" &&
+            typeof err === 'object' &&
             err !== null &&
-            "error" in err &&
-            typeof (err as any).error === "object" &&
+            'error' in err &&
+            typeof (err as any).error === 'object' &&
             (err as any).error !== null &&
-            "status" in (err as any).error &&
+            'status' in (err as any).error &&
             (err as any).error.status === 404
           ) {
             dispatch(setExpiredRentalOrders([]));
             return;
           }
-          console.error("Failed to fetch customers:", err);
+          console.error('Failed to fetch customers:', err);
         }
       },
     }),
@@ -76,27 +72,34 @@ export const contactApi = rootApi.injectEndpoints({
       query: ({ id, payload }) => {
         return {
           url: `orders/rentals/${id}`,
-          method: "PATCH",
+          method: 'PATCH',
           body: payload,
         };
       },
-      invalidatesTags: ["Rental"],
+      invalidatesTags: ['Rental'],
       async onQueryStarted({ payload }, { dispatch, queryFulfilled }) {
         try {
           await queryFulfilled;
           // Check if the patched field includes "in_date"
           const inDatePatched = payload.some(
-            (op) => op.path === "/rental_duration" || op.path === "/status"
+            (op) => op.path === '/rental_duration' || op.path === '/status'
           );
 
           if (inDatePatched) {
             // Manually invalidate or trigger related query
-            dispatch(rootApi.util.invalidateTags(["Expired"])); // or use your specific tag/query
+            dispatch(rootApi.util.invalidateTags(['Expired'])); // or use your specific tag/query
           }
         } catch (err) {
-          console.error("Patch failed:", err);
+          console.error('Patch failed:', err);
         }
       },
+    }),
+    postOrderDcAsWhatsappMessage: build.mutation<string, DCWhatsappPayload>({
+      query: (body) => ({
+        url: 'orders/rentals/whatsapp-dc',
+        method: 'POST',
+        body: { body },
+      }),
     }),
   }),
 });

c9818693de21c5616ba7040082889bc8f09a4960
Cherisher08 <vigu2002ram@gmail.com>
2025-10-10 14:35:29 +0530

    refactor: :green_heart: Fix build issues

--- src/pages/private/Entries/NewOrder.tsx
+++ src/pages/private/Entries/NewOrder.tsx
@@ -9,7 +9,7 @@ import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import Loader from '../../../components/Loader';
-import SplitOrdermodal from '../../../components/SplitOrderModal';
+import SplitOrdermodal from '../../../components/SplitOrderModals';
 import { TOAST_IDS } from '../../../constants/constants';
 import {
   useGetProductsQuery,
